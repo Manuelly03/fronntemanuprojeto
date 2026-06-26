1a9a2a48902ad36074ede4dd9dc446d379bed1ea
Manuelly03 <fernandesmanuelly341@gmail.com>
2026-06-26 08:38:47 -0300

    finalizei finalmentee

--- src/app/(public)/informations/page.js
+++ src/app/(public)/informations/page.js
@@ -4,7 +4,7 @@ export default function Informations() {
       <h1 className="text-2xl font-bold mb-2">Informações</h1>
       <p className="text-sm">
         Somos uma página dedicada a avaliações de filmes. Aqui você pode encontrar opiniões,
-        notas e críticas sobre diversos filmes. Também reunimos avaliações de outras pessoas,
+        notas e críticas sobre diversos filmes e avaliar também. Também reunimos avaliações de outras pessoas,
         para que você veja diferentes pontos de vista antes de assistir.
       </p>
     </div>
